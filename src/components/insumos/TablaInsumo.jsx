1db662c2e68ceb7bf95bd7adfd371a9d020e4a9e
AnnerMonge <jadircruz750@gmail.com>
2025-10-21 16:54:13 -0600

    Tabla Insumo Programada en (Components)Monge_Cruz

---
+++ src/components/insumos/TablaInsumo.jsx
@@ -0,0 +1,43 @@
+import { Table, Spinner } from "react-bootstrap";
+
+const TablaInsumos = ({ insumos, cargando }) => {
+  if (cargando) {
+    return (
+      <>
+        <Spinner animation="border">
+          <span className="visually-hidden">Cargando...</span>
+        </Spinner>
+      </>
+    );
+  }
+  return (
+    <>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID Insumo</th>
+          <th>Fecha Insumo</th> 
+          <th>Total Insumo</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {insumos.map((insumo) => {
+          return (
+          <tr key={insumo.idInsumo}>
+            <td>{insumo.id_insumo}</td>
+            <td>{insumo.fecha_insumo}</td>
+            <td>{insumo.total_insumo}</td>
+            <td>Acción</td>
+          </tr>
+            );    
+       })}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+export default TablaInsumos;
+
+
+    
--- src/views/Insumos.jsx
+++ src/views/Insumos.jsx
@@ -1,8 +1,36 @@
-const Insumos= () => {
-return (
-<>
-<h2>Página de Insumos</h2>
-</>
-);
-}
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import TablaInsumos from "../components/insumos/TablaInsumo";
+
+const Insumos = () => {
+  const [insumos, setinsumos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+
+  const obtenerProductos = async () => {
+    try {
+      const respuesta = await fetch("http://localhost:3000/api/insumo");
+      if (!respuesta.ok) {
+        throw new Error("Error al obtener los insumos");
+      }
+      const datos = await respuesta.json();
+      setinsumos(datos);
+      setCargando(false);
+    } catch (error) {
+      console.error(error.message);
+      setCargando(false);
+    }
+  };
+
+  useEffect(() => {
+    obtenerProductos();
+  }, []);
+  return (
+    <>
+      <Container className="mt-4">
+        <h4> Insumos </h4>
+        <TablaInsumos insumos={insumos} cargando={cargando} />
+      </Container>
+    </>
+  );
+};
 export default Insumos;
