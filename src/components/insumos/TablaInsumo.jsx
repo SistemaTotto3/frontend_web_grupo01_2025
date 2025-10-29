7e21c0ee1a96d1e46cd581d88914d94694a45725
AnnerMonge <jadircruz750@gmail.com>
2025-10-29 15:08:17 -0600

    BotonOrden Agregado y el ordenamiento a insumo programado

--- src/components/insumos/TablaInsumo.jsx
+++ src/components/insumos/TablaInsumo.jsx
@@ -1,6 +1,32 @@
 import { Table, Spinner } from "react-bootstrap";
+import { useState } from "react";
+import BotonOrden from "../ordenamiento/BotonOrden";
 
 const TablaInsumos = ({ insumos, cargando }) => {
+  const [orden, setOrden] = useState({
+      campo: "id_insumo",
+      direccion: "asc",
+    });
+    const manejarOrden = (campo) => {
+      setOrden((prev) => ({
+        campo,
+        direccion:
+          prev.campo === campo && prev.direccion === "asc" ? "desc" : "asc",
+      }));
+    };
+  
+    const insumosOrdenados = [...insumos].sort((a, b) => {
+      const valorA = a[orden.campo];
+      const valorB = b[orden.campo];
+  
+      if (typeof valorA === "number" && typeof valorB === "number") {
+        return orden.direccion === "asc" ? valorA - valorB : valorB - valorA;
+      }
+  
+      const comparacion = String(valorA).localeCompare(String(valorB));
+      return orden.direccion === "asc" ? comparacion : -comparacion;
+    });
+
   if (cargando) {
     return (
       <>
@@ -15,14 +41,32 @@ const TablaInsumos = ({ insumos, cargando }) => {
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>ID Insumo</th>
-          <th>Fecha Insumo</th> 
-          <th>Total Insumo</th>
+          <BotonOrden
+              campo="id_insumo"
+              orden={orden}
+              manejarOrden={manejarOrden}
+            >
+              ID Insumo
+            </BotonOrden>
+            <BotonOrden
+              campo="fecha_insumo"
+              orden={orden}
+              manejarOrden={manejarOrden}
+            >
+              Fecha Insumo
+            </BotonOrden>
+            <BotonOrden
+              campo="total_insumo"
+              orden={orden}
+              manejarOrden={manejarOrden}
+            >
+              Total Insumo
+            </BotonOrden>
           <th>Acciones</th>
         </tr>
       </thead>
       <tbody>
-        {insumos.map((insumo) => {
+        {insumosOrdenados.map((insumo) => {
           return (
           <tr key={insumo.id_insumo}>
             <td>{insumo.id_insumo}</td>
